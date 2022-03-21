65f346c75440287a346abfa0b2c9d4612506a2d8
AlexandraBindiu <102045631+AlexandraBindiu@users.noreply.github.com>
2022-03-21 17:19:57 +0200

    Add files via upload

---
+++ Medicament.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace LibrarieModele
+{
+    public class Medicament
+    {
+        //constante
+        private const char SEPARATOR_PRINCIPAL_FISIER = ';';
+
+        private const int ID = 0;
+        private const int DENUMIRE = 1;
+        private const int PRET = 2;
+        private const int PRODUCATOR = 3;
+        private const int VALABILITATE = 4;
+
+        //proprietati auto-implemented
+        private int idMedicament; //identificator unic medicament
+        private string denumire;
+        private string pret;
+        private string producator;
+        private string valabilitate;  //anul in care medicamentul expira
+
+        //contructor implicit
+        public Medicament()
+        {
+            denumire = producator = pret =valabilitate= string.Empty;
+            //pret = 0;
+            //valabilitate = 0;
+        }
+
+        //constructor cu parametri
+        public Medicament(int idMedicament, string denumire, string pret, string producator, string valabilitate)
+        {
+            this.idMedicament = idMedicament;
+            this.denumire = denumire;
+            this.pret = pret;
+            this.producator = producator;
+            this.valabilitate = valabilitate;
+        }
+
+        //constructor cu un singur parametru de tip string care reprezinta o linie dintr-un fisier text
+        public Medicament(string linieFisier)
+        {
+            var dateFisier = linieFisier.Split(SEPARATOR_PRINCIPAL_FISIER);
+
+            //ordinea de preluare a campurilor este data de ordinea in care au fost scrise in fisier prin apelul implicit al metodei ConversieLaSir_PentruFisier()
+            idMedicament = Convert.ToInt32(dateFisier[ID]);
+            denumire = dateFisier[DENUMIRE];
+            pret = dateFisier[PRET];
+            producator = dateFisier[PRODUCATOR];
+            valabilitate = dateFisier[VALABILITATE];
+        }
+
+        public string ConversieLaSir_PentruFisier()
+        {
+            string obiectMedicamentPentruFisier = string.Format("{1}{0}{2}{0}{3}{0}{4}{0}{5}{0}",
+                   SEPARATOR_PRINCIPAL_FISIER,
+                   idMedicament.ToString(),
+                   denumire.ToString(),
+                   pret.ToString(),
+                   producator.ToString(),
+                   valabilitate.ToString());
+
+            return obiectMedicamentPentruFisier;
+        }
+        public int GetIdMedicament()
+        {
+            return idMedicament;
+        }
+
+        public string GetDenumire()
+        {
+            return denumire;
+        }
+        public string GetPret()
+        {
+            return pret;
+        }
+        public string GetProducator()
+        {
+            return producator;
+        }
+        public string GetValabilitate()
+        {
+            return valabilitate;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Configuration;
+using LibrarieModele;
+using NivelStocareDate;
+
+namespace GestiuneFarmacie
+{
+    class Program
+    {
+        static void Main()
+        {
+            string numeFisier = ConfigurationManager.AppSettings["NumeFisier"];
+            AdministrareMedicament_FisierText adminMedicamente = new AdministrareMedicament_FisierText(numeFisier);
+            int nrMedicamente = 0;
+            string denumireNoua = " ";
+            string pretNou = "";
+            string producatorNou = " ";
+            string valabilitateNoua = "";
+            string Denumire = "";
+            Medicament existingMedicament = new Medicament();
+
+
+            string optiune;
+            do
+            {
+                Console.WriteLine("F. Afisare medicamente din fisier");
+                Console.WriteLine("S. Salvare medicament in fisier");
+                Console.WriteLine("C. Adauga medicament de la tastatura");
+                Console.WriteLine("L. cauta medicament dupa denumire");
+                Console.WriteLine("X. Inchidere program");
+                Console.WriteLine("Alegeti o optiune");
+                optiune = Console.ReadLine();
+                switch (optiune.ToUpper())
+                {
+                    case "F":
+
+                        Medicament[] medicamente = adminMedicamente.GetMedicamente(out nrMedicamente);
+                        AfisareMedicamente(medicamente, nrMedicamente);
+                        break;
+                    case "S":
+                        int idMedicament = adminMedicamente.GetLastMedicamentId() + 1;
+                        //int idMedicament = nrMedicamente + 1;
+                        nrMedicamente = nrMedicamente + 1;
+
+                        //Medicament medicament = new Medicament(idMedicament, "Paracetamol", "15.7", "Andronescu", "2023");
+                        
+                        Medicament medicament = new Medicament();
+                        if (denumireNoua != "" || pretNou!= "" || producatorNou != "" || valabilitateNoua != "")
+                        {
+                            //salvam medicamentul introdus
+                            medicament = new Medicament(idMedicament, denumireNoua, pretNou, producatorNou,valabilitateNoua);
+                        }
+                        else
+                        {
+                            medicament = new Medicament(idMedicament, "Ibuprofen", "34.9", "Danem", "2024");
+                        }
+
+                        //adaugare student in fisier
+                        adminMedicamente.AddMedicament(medicament);
+                        break;
+                    case "C":
+                        Console.WriteLine("Introduceti denumirea medicamentului: ");
+                        denumireNoua = Console.ReadLine();
+                        Console.WriteLine("Introduceti pretul medicamentului: ");
+                        pretNou = Console.ReadLine();
+                        // pretNou = Convert.ToInt32(pretNou);
+                        Console.WriteLine("Introduceti producatorul medicamentului: ");
+                        producatorNou = Console.ReadLine();
+                        Console.WriteLine("Introduceti anul de valabilitate a medicamentului: ");
+                        valabilitateNoua = Console.ReadLine();
+                        break;
+                    case "L":
+                        Console.WriteLine("Introduti denumirea medicamentului cautat:");
+                        Denumire = Console.ReadLine();
+                        existingMedicament = adminMedicamente.GetMedicament(Denumire);
+                        if(existingMedicament == null)
+                        {
+                            Console.WriteLine("Medicamentul cautat nu exista!");
+                        }
+                        else
+                        {
+                            Console.WriteLine(string.Format("Medicamentul cautat are id-ul #{0} si pretul {1}",
+                                existingMedicament.GetIdMedicament(),
+                                existingMedicament.GetPret() ?? "NECUNOSCUT"));
+                        }
+                        
+                        break;
+                    case "X":
+                        return;
+                    default:
+                        Console.WriteLine("Optiune inexistenta");
+                        break;
+                }
+            } while (optiune.ToUpper() != "X");
+
+            Console.ReadKey();
+        }
+
+        public static void AfisareMedicamente(Medicament[] medicamente, int nrMedicamente)
+        {
+            Console.WriteLine("Medicamentele sunt:");
+            for (int contor = 0; contor < nrMedicamente; contor++)
+            {
+                string infoMedicament = string.Format("Medicamentul cu id-ul #{0} are denumirea: {1}, pretul {2}, producatorul {3} si este valabil pana in anul {4}",
+                   medicamente[contor].GetIdMedicament(),
+                   medicamente[contor].GetDenumire() ?? " NECUNOSCUT ",
+                   medicamente[contor].GetPret(),
+                   medicamente[contor].GetProducator() ?? " NECUNOSCUT ",
+                   medicamente[contor].GetValabilitate());
+
+                Console.WriteLine(infoMedicament);
+            }
+        }
+    }
+}
